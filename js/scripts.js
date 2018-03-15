96a12cf4aefa260258d2474b9581d9e0a3305d96
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-03-15 11:53:02 +0100

    Add pagination
    Add unobstrusive dynamic pagination system, with functions that could take care of any amount of students

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-var studentListArray = [].slice.call(document.querySelectorAll(".student-item"));
-var studentNumberByPage = 10;
+var studentListArray = [].slice.call(document.querySelectorAll('.student-item')),
+    totalStudentInList = studentListArray.length,
+    totalStudentByPage = 10,
+    totalPaginationLinks = Math.floor(totalStudentInList / totalStudentByPage);
+    paginationHtml = '';
+    paginationSection =  document.getElementById('paginationOutputSection');
 
-function hideStudents(numberTohide, event){
+// Function to hide students
+function hideStudents(totalToHide, event) {
     var slicedArray;
-    slicedArray = studentListArray.slice(numberTohide, studentListArray.length);
+    slicedArray = studentListArray.slice(totalToHide, totalStudentInList);
     for(var i = 0; i < slicedArray.length; i += 1) {
-        slicedArray[i].style.display = "none";
+        slicedArray[i].style.display = 'none';
     }
 }
 
+// Function to create pagination links
+function createPaginationLinks (numberOfPages, event) {
+    paginationHtml += '<div class="pagination"><ul>';
+        for (var i = 1; i <= numberOfPages; i += 1) {
+            paginationHtml += '<li><a href="#">'+i+'</a>';
+        }
+    paginationHtml += '</div></ul>';
+    paginationSection.innerHTML = paginationHtml;
+}
+
+// Event listener to dynamically hide students & create pagination links
 window.addEventListener("load", function(event){
-    hideStudents(studentNumberByPage, event);
+    hideStudents(totalStudentByPage, event);
+    createPaginationLinks(totalPaginationLinks);
 });
