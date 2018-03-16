21c69b176a2d6afe446c7b273ed7ff0bf2c69b5d
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-03-16 14:22:21 +0100

    Pagination link filtering content
    Added pagination link to show each pages of 10 items... I also rename a lot of my javascript function and variables to make it more readable and generic (and maybe i can reuse that code for a public pagination project)

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,76 @@
-var studentListArray = [].slice.call(document.querySelectorAll('.student-item')),
-    totalStudentInList = studentListArray.length,
-    totalStudentByPage = 10,
-    totalPaginationLinks = Math.floor(totalStudentInList / totalStudentByPage);
-    paginationHtml = '';
+var itemList = document.querySelectorAll('.student-item'),
+    itemListArray = [].slice.call(itemList),
+    totalItemInList = itemListArray.length,
+    totalItemByPage = 10,
+    totalPaginationLinks = Math.ceil(totalItemInList / totalItemByPage),
+    paginationHtml = '',
+    slicedArray = [],
     paginationSection =  document.getElementById('paginationOutputSection');
 
-// Function to hide students
-function hideStudents(totalToHide, event) {
-    var slicedArray;
-    slicedArray = studentListArray.slice(totalToHide, totalStudentInList);
+// Function to hide items on first load
+function hideListItems(totalToHide) {
+    slicedArray = itemListArray.slice(totalToHide, totalItemInList);
     for(var i = 0; i < slicedArray.length; i += 1) {
         slicedArray[i].style.display = 'none';
     }
 }
 
 // Function to create pagination links
-function createPaginationLinks (numberOfPages, event) {
+function createPaginationLinks (numberOfPageItem) {    
     paginationHtml += '<div class="pagination"><ul>';
-        for (var i = 1; i <= numberOfPages; i += 1) {
-            paginationHtml += '<li><a href="#">'+i+'</a>';
+        for (var i = 1; i <= numberOfPageItem; i += 1) {
+            paginationHtml += '<li><a class="pagination-link" href="#">'+i+'</a>';
         }
     paginationHtml += '</div></ul>';
     paginationSection.innerHTML = paginationHtml;
+
+    var paginationLink = document.querySelectorAll('.pagination-link');
+    // Event listener to dynamically hide students & create pagination links
+    for (var i = 0; i < paginationLink.length;  i+= 1) { 
+        paginationLink[i].addEventListener("click", function(event){
+            // Prevent defautl behaviour on click
+            event.preventDefault();
+            var pageNumber = parseInt(event.target.innerText),
+                itemListStart = pageNumber * totalItemByPage - totalItemByPage,
+                itemListEnd = itemListStart + totalItemByPage;            
+            
+            // We have a different behaviour if the clicked button is the last
+            if (pageNumber === totalItemByPage) {
+                itemListStart = pageNumber * totalItemByPage;
+                itemListEnd = itemListStart + totalItemByPage + totalItemByPage;
+            }
+            // We get & hide visible item
+            getItemToHide();
+            
+            // We get and hide next XXX items
+            getItemToShow(itemListStart, itemListEnd);
+        });
+    }
+}
+
+// Function to get the item to hide
+function getItemToHide() {
+    var visibleItems = document.querySelectorAll('.student-item:not([style="display: none;"])');
+    for(var i = 0; i < visibleItems.length; i += 1) {
+        visibleItems[i].style.display = 'none';
+    }
+}
+
+// Function to get the item to show
+function getItemToShow (start, end) {
+    slicedArray = itemListArray.slice(start, end);
+    for(var i = 0; i < slicedArray.length; i += 1) {
+        slicedArray[i].style.display = 'block';
+    }
 }
 
 // Event listener to dynamically hide students & create pagination links
-window.addEventListener("load", function(event){
-    hideStudents(totalStudentByPage, event);
+window.addEventListener('load', function(event){
+    // We hide list items 
+    hideListItems(totalItemByPage);
+    // We create and add pagination links
     createPaginationLinks(totalPaginationLinks);
 });
+
+
+
