2ba0d9a65e7ff6688ae0e20e0e4cf634bb0129ce
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-03-20 13:27:52 +0100

    Finish script for searchbar
    Now show and hide search student list, filter and display result according name and email comparaison during search and display a "no result message" when the matches array is empty. I did not achivee to make dynamical pagiantion according those results... And I have no time to implet it now because I've to move on with next unit (i'm doing that formation on my work time, it is paid by my start-up).

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var itemList = document.querySelectorAll('.student-item'),
     totalPaginationLinks = Math.ceil(totalItemInList / totalItemByPage),    
     slicedArray = [];
 
-// Function to print Html
+    // Function to print Html
 function printHtml(htmlMarkup, output) {
     output.innerHTML = htmlMarkup;
 }
@@ -20,7 +20,7 @@ function hideListItems(totalToHide) {
 
 // Function to create pagination links
 function addPagination (numberOfPageItem) {
-    var paginationTarget =  document.getElementById('paginationOutputSection'),
+    var paginationTarget = document.getElementById('paginationOutputSection'),
         paginationHtml = '';    
 
     paginationHtml += '<div class="pagination"><ul>';
@@ -50,7 +50,7 @@ function addPagination (numberOfPageItem) {
             getItemToHide();
             
             // We get and hide next XXX items
-            getItemToShow(itemListStart, itemListEnd);
+            getItemToShow(itemListArray, itemListStart, itemListEnd);
         });
     }
 }
@@ -64,42 +64,78 @@ function addSearch () {
     searchbarHtml += '<input id="searchInput" placeholder="Search for students...">';
     searchbarHtml += '<button id="searchBtn">Search</button>';
     searchbarHtml += '</div>';
+
+    // We call the function to print the searchbar HTML
     printHtml(searchbarHtml, searchbarTarget);
 
     var searchBtn = document.getElementById('searchBtn'),
         searchInput = document.getElementById('searchInput');
-    var switchBool = true;
 
-    searchBtn.addEventListener("click", function(event){
+    // Watch click on search button
+    searchBtn.addEventListener("click", function(){
         var array = [],
-            searchInputValue = document.getElementById('searchInput').value;
+            searchInputValue = document.getElementById('searchInput').value,
+            studentList = document.getElementById('studentList'),
+            noSearchResultMessage = document.getElementById('noSearchResultMessage'),
+            paginationOutputSection = document.getElementById('paginationOutputSection');
 
+        // We create an object and set name, email, element and we push it to an array
         for(var i = 0; i < itemListArray.length; i += 1) {
             var obj = {};
             obj['name'] = itemListArray[i].querySelector('h3').innerText,
-            obj['email'] = itemListArray[i].querySelector('.email').innerText               
+            obj['email'] = itemListArray[i].querySelector('.email').innerText,
+            obj['element'] = itemListArray[i].querySelector('.email').parentElement.parentElement;
             array.push(obj);
         }
-
+        
+        // We check seatch input emptiness
         if(searchInputValue != '') {
-            console.log('not empty');
-        } else {
-            console.log('empty');
+            // If not empty we create an array and push the search results in it
+            var arrayMatchSearch = [];
+            for(var i = 0; i < array.length; i += 1) {
+                if(array[i].name.includes(searchInputValue) || array[i].email.includes(searchInputValue)) {
+                    arrayMatchSearch.push(array[i].element);
+                }
+            }
+
+            // We hide all items
+            getItemToHide();
+
+            // We check for the pagination being displayed or not according to the result array length (i do not achived to make pagination dynamic according to those results... :/)
+            if(arrayMatchSearch.length >= totalItemByPage) {
+                paginationOutputSection.style.display = 'block';
+            } else {
+                paginationOutputSection.style.display = 'none';                
+            }
+           
+             // We check if the result matches array is empty
+            if(arrayMatchSearch != '') {
+                // If not empty, we display the items according to the search matching array
+                noSearchResultMessage.style.display = 'none';
+                studentList.style.display = 'block';
+                getItemToShow(arrayMatchSearch);
+            } else {
+                // Else: we show a search error message
+                studentList.style.display = 'none';
+                noSearchResultMessage.style.display = 'block';
+                noSearchResultMessage.innerHTML = '<p>No student matches your search, try again !</p>';
+            }
         }
     });
 }
 
 // Function to get the item to hide
 function getItemToHide() {
-    var visibleItems = document.querySelectorAll('.student-item:not([style="display: none;"])');
-    for(var i = 0; i < visibleItems.length; i += 1) {
-        visibleItems[i].style.display = 'none';
-    }
+        var visibleItems;
+        visibleItems = document.querySelectorAll('.student-item:not([style="display: none;"])');
+        for(var i = 0; i < visibleItems.length; i += 1) {
+            visibleItems[i].style.display = 'none';
+        }
 }
 
 // Function to get the item to show
-function getItemToShow (start, end) {
-    slicedArray = itemListArray.slice(start, end);
+function getItemToShow (array, start, end) {
+    slicedArray = array.slice(start, end);
     for(var i = 0; i < slicedArray.length; i += 1) {
         slicedArray[i].style.display = 'block';
     }
